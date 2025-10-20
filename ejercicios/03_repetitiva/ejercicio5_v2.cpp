468e967b8a3b92584efc91059fb58043a1d6eeea
José Domingo Muñoz <josedom24@gmail.com>
2025-10-20 11:46:53 +0200

    modulo03

--- ejercicios/03_repetitiva/ejercicio5_v2.cpp
+++ ejercicios/03_repetitiva/ejercicio5_v2.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int main() {
-    int num;
-    cout << "Introduce un número para mostrar su tabla de multiplicar: ";
-    cin >> num;
-
-    for (int i = 1; i <= 10; ++i) {
-        cout << num << " x " << i << " = " << num * i << '\n';
+    // Mostrar las tablas de multiplicar del 1 al 5
+    for (int num = 1; num <= 5; ++num) {
+        cout << "\nTabla del " << num << ":\n";
+        cout << "-----------------\n";
+        for (int i = 1; i <= 10; ++i) {
+            cout << num << " x " << i << " = " << num * i << '\n';
+        }
     }
 
     return 0;
