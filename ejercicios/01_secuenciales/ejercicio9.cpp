15db0c973504aed7667992f4ae3d3de60efb9993
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2025-11-17 15:41:28 +0100

    Rename descuento to DESCUENTO and update usage

--- ejercicios/01_secuenciales/ejercicio9.cpp
+++ ejercicios/01_secuenciales/ejercicio9.cpp
@@ -2,15 +2,15 @@
 
 int main() {
     auto total_compra {0.0};
-    constexpr double descuento {0.15};
+    constexpr double DESCUENTO {0.15};
 
     std::cout << "Introduce el total de la compra: ";
     std::cin >> total_compra;
 
-    auto monto_descuento = total_compra * descuento;
-    auto total_final = total_compra - monto_descuento;
+    auto cantidad_descuento = total_compra * DESCUENTO;
+    auto total_final = total_compra - cantidad_descuento;
 
-    std::cout << "Descuento aplicado: " << monto_descuento << std::endl;
+    std::cout << "Descuento aplicado: " << cantidad_descuento << std::endl;
     std::cout << "Total a pagar: " << total_final << std::endl;
 
     return 0;
